5073afc20f045ce77f1a51e6b4b1711b55fdf969
Tyler Maschino <Tylermaschino@gmail.com>
2018-10-27 18:09:25 -0400

    Added functionality to input ideas and show a neat hat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import PersonPicker from "./components/PersonPicker";
+import Scroll from "./components/Scroll";
+import Hat from "./components/Hat";
 
 class App extends Component {
+  state = {
+    stage: 0,
+    people: 0,
+    currentIdea: "",
+    ideas: []
+  };
+
+  nextStage = e => {
+    this.setState(state => {
+      return {
+        stage: state.stage + 1
+      };
+    });
+  };
+
+  updatePeople = e => {
+    this.setState({
+      people: +e.target.value
+    });
+  };
+
+  updateCurrentIdea = e => {
+    this.setState({
+      currentIdea: e.target.value
+    });
+  };
+
+  submitIdea = () => {
+    this.setState(
+      prevState => {
+        return {
+          ideas: [...prevState.ideas, prevState.currentIdea],
+          currentIdea: ""
+        };
+      },
+      () => {
+        console.log("callback");
+        if (this.state.ideas.length === this.state.people) {
+          this.nextStage();
+        }
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Group Think</h1>
+        {/* Craptastic routing */}
+        {this.state.stage === 0 ? (
+          <LandingPage nextStage={this.nextStage} />
+        ) : null}
+        {this.state.stage === 1 ? (
+          <PersonPicker
+            updatePeople={this.updatePeople}
+            nextStage={this.nextStage}
+          />
+        ) : null}
+        {this.state.stage === 2 ? (
+          <Scroll
+            people={this.state.people}
+            currentIdea={this.state.currentIdea}
+            updateCurrentIdea={this.updateCurrentIdea}
+            submitIdea={this.submitIdea}
+            ideasLength={this.state.ideas.length}
+          />
+        ) : null}
+        {this.state.stage === 3 ? <Hat /> : null}
       </div>
     );
   }
