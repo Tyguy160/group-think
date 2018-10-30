f7a32b15d43293c9bd817282aec3615f7c3b0088
Tyler Maschino <tylermaschino@gmail.com>
2018-10-29 20:40:21 -0400

    Changed styling

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -7,14 +7,8 @@ function Voting({ elimination, countVotes, ideas, vote }) {
     <div>
       <h2>Vote!</h2>
       <p>
-<<<<<<< HEAD
-        Person{" "}
-        {props.countVotes(props.elimination ? "negVotes" : "posVotes") + 1}:
-        Vote for {voteType}:
-=======
-        Person {countVotes(elimination ? "negVotes" : "posVotes") + 1} vote for{" "}
-        {voteType}:
->>>>>>> 9f9715e350b8f03c53d8e49cab5ba394d215c612
+        Person {countVotes(elimination ? "negVotes" : "posVotes") + 1}: Vote for{" "}
+        {voteType}
       </p>
       {ideas.map((idea, index) => {
         return (
